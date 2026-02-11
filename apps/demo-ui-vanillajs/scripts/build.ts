39ac1a1b86ba11246e9b866c70fed2e0896b790f
Steve Landey <steve@steveasleep.com>
2026-02-11 12:29:24 -0800

    remove renderer-konva, ui-vanillajs, and demo-ui app

--- apps/demo-ui-vanillajs/scripts/build.ts
+++
@@ -1,5 +0,0 @@
-import { $ } from "bun";
-
-await $`rm -rf dist`;
-await $`mkdir -p dist`;
-await $`bun build src/index.html --outdir dist --target=browser`;
--- packages/renderer-konva/src/__tests__/setup.ts
+++
@@ -1,9 +0,0 @@
-import { automergeWasmBase64 } from "@automerge/automerge/automerge.wasm.base64";
-import {
-  initializeBase64Wasm,
-  isWasmInitialized,
-} from "@automerge/automerge/slim";
-
-if (!isWasmInitialized()) {
-  await initializeBase64Wasm(automergeWasmBase64);
-}
